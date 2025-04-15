311f3db4933cbeda7dc0de30e871fb9d7c8aece6
kosemeci <kosemecialperen37@gmail.com>
2025-04-16 01:56:32 +0300

    created user,address and coupon entities

---
+++ backend/src/main/java/com/kosemeci/ecommerce/entity/Coupon.java
@@ -0,0 +1,22 @@
+package com.kosemeci.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
---
+++ backend/src/main/java/com/kosemeci/ecommerce/entity/User.java
@@ -0,0 +1,52 @@
+package com.kosemeci.ecommerce.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kosemeci.ecommerce.domain.USER_ROLE;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    //gösterilen apilerde gözükmemesi için
+    private String password;
+
+    private String email;
+
+    private String fullName;
+
+    private String mobile;
+
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
+
+    @OneToMany
+    private Set<Address> addresses = new HashSet<>();
+
+    @ManyToMany
+    @JsonIgnore //Bir alanı JSON çıktısından tamamen gizlemek için kullanılır.
+    private Set<Coupon> coupons = new HashSet<>();
+
+}
