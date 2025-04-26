e27cb7c1ca2bef6dd7ad52e289a92bc6ab9dd47b
kosemeci <kosemecialperen37@gmail.com>
2025-04-26 14:57:27 +0300

    create starter packages for app

--- backend/src/main/java/com/kosemeci/ecommerce/starter/EcommerceApplication.java
+++ backend/src/main/java/com/kosemeci/ecommerce/starter/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.kosemeci.ecommerce;
+package com.kosemeci.ecommerce.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
