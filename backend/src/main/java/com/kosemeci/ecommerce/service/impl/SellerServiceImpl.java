08f1948e6dbc3b9aaf51c046c5355624efc63e4a
kosemeci <kosemecialperen37@gmail.com>
2025-04-29 00:22:37 +0300

    create address repository

--- backend/src/main/java/com/kosemeci/ecommerce/service/impl/SellerServiceImpl.java
+++ backend/src/main/java/com/kosemeci/ecommerce/service/impl/SellerServiceImpl.java
@@ -2,66 +2,98 @@ package com.kosemeci.ecommerce.service.impl;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.kosemeci.ecommerce.config.JwtProvider;
 import com.kosemeci.ecommerce.domain.AccountStatus;
+import com.kosemeci.ecommerce.domain.USER_ROLE;
 import com.kosemeci.ecommerce.entity.Seller;
+import com.kosemeci.ecommerce.repository.SellerRepository;
 import com.kosemeci.ecommerce.service.SellerService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SellerServiceImpl implements SellerService{
+
+    private final SellerRepository sellerRepository;
+    private final JwtProvider jwtProvider;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Seller getSellerProfile(String token) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSellerProfile'");
+
+        String email = jwtProvider.getEmailFromJwtToken(token);
+        return getSellerByEmail(email);
     }
 
     @Override
     public Seller createSeller(Seller seller) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createSeller'");
+        Seller sellerExist = sellerRepository.findByEmail(seller.getEmail());
+        if(sellerExist!=null){
+            try {
+                throw new Exception("Seller already created with this email. Try another email or login.");
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        Seller newSeller = new Seller();
+        newSeller.setEmail(seller.getEmail());
+        newSeller.setRole(USER_ROLE.ROLE_SELLER);
+
+        return newSeller;
+
     }
 
     @Override
     public Seller getSellerById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSellerById'");
+
+        Seller seller = sellerRepository.findById(id).orElseThrow(()->new Exception("dont found with this id" + id));
     }
 
     @Override
     public Seller getSellerByEmail(String email) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSellerByEmail'");
+
+        Seller seller = sellerRepository.findByEmail(email);
+        try {
+            if(seller==null){
+                throw new Exception("Not found with email - " + email);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return seller;
     }
 
     @Override
     public List<Seller> getAllSellers(AccountStatus status) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'getAllSellers'");
     }
 
     @Override
     public Seller updateSeller(Long id, Seller seller) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'updateSeller'");
     }
 
     @Override
     public void deleteSeller(Long id) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'deleteSeller'");
     }
 
     @Override
     public Seller verifyEmail(String email, String otp) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'verifyEmail'");
     }
 
     @Override
     public Seller updateSellerAccountStatus(Long sellerId, AccountStatus status) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'updateSellerAccountStatus'");
     }
 
